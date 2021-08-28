b4aea07833e1e021448dc006f9d4c495571aef71
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-28 21:52:28 +0530

    ADDED: method to print maximum of three integers

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -2,4 +2,23 @@ package com.bridgelabz.maximumproblem;
 
 public class MaximumProblem {
 
+
+	
+	public static void maxOfThreeNumbers(Integer one, Integer two, Integer three) {
+		
+		Integer maxOfTwo=(one.compareTo(two)>0?one:two);
+		Integer maxInteger=(one.compareTo(three)>0?one:three);
+		System.out.println("Maximum of three integers are:"+maxInteger);
+	}
+	
+public static void main(String[] args) {
+		
+		Integer firstNumber=5;
+		Integer secondNumber=10;
+		Integer thirdNumber=15;
+		
+		maxOfThreeNumbers(firstNumber,secondNumber,thirdNumber);
+
+	}
+
 }
