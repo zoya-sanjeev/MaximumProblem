9ffb5047ddb026675adc2a850fc2b020f1589913
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-28 22:03:34 +0530

    ADDED: method to find maximum of three float number

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -4,20 +4,34 @@ public class MaximumProblem {
 
 
 	
-	public static void maxOfThreeNumbers(Integer one, Integer two, Integer three) {
+	public static void findMaxOfThree(Integer one, Integer two, Integer three) {
 		
 		Integer maxOfTwo=(one.compareTo(two)>0?one:two);
 		Integer maxInteger=(one.compareTo(three)>0?one:three);
 		System.out.println("Maximum of three integers are:"+maxInteger);
 	}
 	
-public static void main(String[] args) {
+	public static void findMaxOfThree(Float one, Float two, Float three) {
 		
-		Integer firstNumber=5;
-		Integer secondNumber=10;
-		Integer thirdNumber=15;
+		Float maxOfTwo=(one.compareTo(two)>0?one:two);
+		Float maxFloat=(one.compareTo(three)>0?one:three);
+		System.out.println("Maximum of three integers are:"+maxFloat);
+	}
+	
+	public static void main(String[] args) {
 		
-		maxOfThreeNumbers(firstNumber,secondNumber,thirdNumber);
+		Integer firstInteger=5;
+		Integer secondInteger=10;
+		Integer thirdInteger=15;
+		
+		findMaxOfThree(firstInteger,secondInteger,thirdInteger);
+		
+		Float firstFloat=1.38f;
+		Float secondFloat=1.33f;
+		Float thirdFloat=1.39f;
+		
+		findMaxOfThree(secondFloat, secondFloat, thirdFloat);
+		
 
 	}
 
