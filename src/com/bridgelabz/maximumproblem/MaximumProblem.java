fba29ca0d1a055cb5e427f0622f7acd95970a9f3
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-29 21:59:19 +0530

    REFACTORED:created generic class to implement maximum problem

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,35 +1,46 @@
 package com.bridgelabz.maximumproblem;
 
-public class MaximumProblem {
+public class MaximumProblem<E extends Comparable<E>> {
 
-
+	E firstValue;
+	E secondValue;
+	E thirdValue;
 	
-	public static <E extends Comparable <E>> void findMaxOfThree(E one, E two, E three) {
-		
-		E maxOfTwo=(one.compareTo(two)>0?one:two);
-		E max=(maxOfTwo.compareTo(three)>0?maxOfTwo:three);
-		System.out.println("Maximum of three integers are:"+max);
+	MaximumProblem(E first,E second,E third){
+		this.firstValue=first;
+		this.secondValue=second;
+		this.thirdValue=third;
 	}
+	
+	public void findMaxOfThree() {
+		
+		E maxOfTwo=(firstValue.compareTo(secondValue)>0?firstValue:secondValue);
+		E max=(maxOfTwo.compareTo(thirdValue)>0?maxOfTwo:thirdValue);
+		System.out.println(max);
+	}
+	
 
 	public static void main(String[] args) {
+		
+		
 		
 		Integer firstInteger=5;
 		Integer secondInteger=10;
 		Integer thirdInteger=15;
 		
-		findMaxOfThree(firstInteger,secondInteger,thirdInteger);
+		new MaximumProblem<Integer>(firstInteger, secondInteger, thirdInteger).findMaxOfThree();
 		
 		Float firstFloat=1.38f;
 		Float secondFloat=1.40f;
 		Float thirdFloat=1.39f;
 		
-		findMaxOfThree(secondFloat, secondFloat, thirdFloat);
+		new MaximumProblem<Float>(firstFloat, secondFloat, thirdFloat).findMaxOfThree();
 		
 		String firstString="apple";
 		String secondString="peach";
 		String thirdString="banana";
 		
-		findMaxOfThree(firstString, secondString, thirdString);
+		new MaximumProblem<String>(firstString, secondString, thirdString).findMaxOfThree();
 		
 
 	}
